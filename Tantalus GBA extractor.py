1cf933892d0649208f83336dd388fef15567277f
Dr. RNG <33909717+BLiNXthetimesweeperGOD@users.noreply.github.com>
2025-01-14 10:06:50 -0500

    Add files via upload

--- Tantalus GBA extractor.py
+++ Tantalus GBA extractor.py
@@ -8,36 +8,35 @@ def uint(data):
     return struct.unpack("<I", data)[0]
 def uintPointer(data):
     return struct.unpack("<I", data)[0]-0x08000000
-
 def openFile():
     root = tk.Tk()
     root.withdraw()
     file = filedialog.askopenfilename()
     root.destroy()
     return file
-
-file = openFile()
-outPath = os.getcwd()+"/"+os.path.splitext(os.path.basename(file))[0]
-if not os.path.exists(outPath):
-    os.makedirs(outPath)
-
 def checkForByteString(path, string):
     with open(path, 'rb') as file:
         content = file.read()
         offset = content.find(string)
         return string in content, offset
 
+file = openFile()
+outPath = os.getcwd()+"/"+os.path.splitext(os.path.basename(file))[0]
+if not os.path.exists(outPath):
+    os.makedirs(outPath)
+
 found, offset = checkForByteString(file, b'\x04\x02\x00\x04\x74\x43\x00\x00')
 
-if found == True:
+if found == True: #The ROM contained the byte string. Start extracting files.
     with open(file, "rb") as rom:
         rom.seek(offset+8)
         tableOffset = uintPointer(rom.read(4))
+        fileTableRootPathOffset = uintPointer(rom.read(4)) #If filenames ever get implemented, this is effectively the root of the ROM ("../targ/")
         rom.seek(tableOffset)
         fileCount = uint(rom.read(4))
-        rom.seek(4, 1)
+        fileTableVersion = uint(rom.read(4)) #Just a guess - this could be something else entirely, but I have no idea yet
         for asset in range(fileCount):
-            rom.seek(4, 1)
+            fileNameHash = uint(rom.read(4)) #This is calculated and stored in register 7 until the correct file in the file table has been found.
             fileOffset = uintPointer(rom.read(4))
             savedPosition = rom.tell()
             rom.seek(fileOffset)
@@ -46,5 +45,5 @@ if found == True:
                 outputFile.write(rom.read(fileSize))
             rom.seek(savedPosition)
 else:
-    print("This ROM is either not from Tantalus Interactive or a bad dump.\nPress enter/return to close this window.")
+    print("The ROM you ran this on is not a valid Tantalus Interactive GBA game.\nThis could be because it's a bad dump, not a ROM or a game from a different developer.\nPress enter/return to close this window.")
     input() #Actually give the user the ability to read this if ran normally
